70f1e754581eeab149486f1a6a953e3de7bf8f77
EgorIvanov96 <shura.1996.ivanov@gmail.com>
2024-09-23 16:39:54 +0300

    Added validation to username

--- library/users/models.py
+++ library/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import RegexValidator
 
 
 class User(AbstractUser):
@@ -17,7 +18,14 @@ class User(AbstractUser):
     username = models.CharField(
         max_length=150,
         verbose_name='Имя пользователя',
-        help_text='Укажите имя пользователя'
+        help_text='Укажите имя пользователя',
+        unique=True,
+        validators=[
+            RegexValidator(
+                r'^[\w.@+-]+\Z',
+                'Поле username содержит недопустимые символы'
+            )
+        ]
     )
 
     class Meta:
